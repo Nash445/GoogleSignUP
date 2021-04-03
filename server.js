373a5e9a291970d68405e873cc7c83161eba6e7e
Nash445 <75974626+Nash445@users.noreply.github.com>
2021-04-03 18:16:23 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.static('public'));
 
 app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile('/index.html');
 })
 app.set('view engine', 'ejs');
 
